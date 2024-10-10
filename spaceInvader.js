f530c532ae9da46ad209cdf3c9d46e09c9e0ed84
Mickaella Batista <mickaella.batista@student.ehb.be>
2024-10-10 11:46:12 +0200

    signature

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,37 +1,21 @@
-Signature();
+drawSignature();
+
 function drawSignature() {
 	let canvas = document.querySelector("canvas");
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 	let context = canvas.getContext("2d");
 
-	context.lineWidth = 5;
 	context.fillStyle = "#FFFAF0";
 	context.beginPath();
 	context.rect(25, 50, 300, 300);
 	context.fill();
 
-	context.lineWidth = 5;
 	context.fillStyle = "green";
 	context.beginPath();
 	context.rect(50, 50, 250, 50);
-	context.fill();
-
-	context.lineWidth = 5;
-	context.fillStyle = "green";
-	context.beginPath();
 	context.rect(50, 150, 50, 100);
-	context.fill();
-
-	context.lineWidth = 5;
-	context.fillStyle = "green";
-	context.beginPath();
 	context.rect(150, 150, 50, 50);
-	context.fill();
-
-	context.lineWidth = 5;
-	context.fillStyle = "green";
-	context.beginPath();
 	context.rect(250, 150, 50, 100);
 	context.fill();
 }
